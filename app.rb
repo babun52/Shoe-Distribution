12eeb05345b47341fd1520d5b69e8e4f35b8e829
James Lee <babun5209@gmail.com>
2016-12-16 16:15:27 -0800

    Add POST method for adding a store on index page.

--- app.rb
+++ app.rb
@@ -8,3 +8,17 @@ get('/') do
   @brands = Brand.all()
   erb(:index)
 end
+
+post('/add_store') do
+  name = params.fetch('store_name')
+  new_store = Store.create(:name => name)
+  @stores = Store.all()
+  erb(:index)
+end
+
+post('/add_brand') do
+  name = params.fetch('brand_name')
+  new_store = Brand.create(:name => name)
+  @brands = Brand.all()
+  erb(:index)
+end
