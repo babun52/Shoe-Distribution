266c3779530510c49ace5206f4ee594d178bbd80
James Lee <babun5209@gmail.com>
2016-12-16 17:30:37 -0800

    Add Update via PATCH for store page.

--- app.rb
+++ app.rb
@@ -9,7 +9,7 @@ get('/') do
   erb(:index)
 end
 
-post('/add_store') do
+post('/stores') do
   name = params.fetch('store_name')
   new_store = Store.create(:name => name)
   @stores = Store.all()
@@ -17,14 +17,25 @@ post('/add_store') do
 end
 
 get('/stores/:id') do
-  @store = Store.find(params.fetch("id").to_i())
+  @store = Store.find(params.fetch('id').to_i)
   @brands = Brand.all()
   erb(:store)
 end
 
-post('/add_brand') do
+patch('/stores/:id') do
+  rename = params.fetch('rename')
+  @store = Store.find(params.fetch('id').to_i)
+  if @store.update({:name => rename})
+    @brands = Brand.all()
+    redirect to ("/stores/#{@store.id()}")
+  else
+    redirect to ("/stores/#{@store.id()}")
+  end
+end
+
+post('/brands') do
   name = params.fetch('brand_name')
-  new_store = Brand.create(:name => name)
+  new_brand = Brand.create(:name => name)
   @brands = Brand.all()
   erb(:store)
 end
